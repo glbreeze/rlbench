ed11179d5a0a5155d8c192737b1f6f15f195f898
glbreeze <gl.breeze@gmail.com>
2026-01-07 15:06:59 +0800

    Add physics: mass, CoM, inertia, friction

---
+++ rlbench/dataset_generator_single.py
@@ -0,0 +1,450 @@
+import argparse
+import json
+import os
+import pickle
+
+import numpy as np
+from PIL import Image
+from pyrep.const import RenderMode
+from pyrep.objects.shape import Shape
+
+import rlbench.backend.task as task
+from rlbench import ObservationConfig
+from rlbench.action_modes.action_mode import MoveArmThenGripper
+from rlbench.action_modes.arm_action_modes import JointVelocity
+from rlbench.action_modes.gripper_action_modes import Discrete
+from rlbench.backend import utils
+from rlbench.backend.const import *
+from rlbench.backend.utils import task_file_to_task_class
+from rlbench.environment import Environment
+from rlbench.helper import inject_task_physics, simGetShapeMassAndInertia
+from pyrep.backend import sim
+import time
+from pyrep.objects.dummy import Dummy
+from pyrep.objects.vision_sensor import VisionSensor
+
+from pyrep.objects.dummy import Dummy
+import numpy as np
+
+def T12_to_R_t(T12):
+    T12 = np.asarray(T12, float).reshape(12)
+    R = np.array([
+        [T12[0],  T12[1],  T12[2]],
+        [T12[4],  T12[5],  T12[6]],
+        [T12[8],  T12[9],  T12[10]],
+    ], dtype=float)
+    t = np.array([T12[3], T12[7], T12[11]], dtype=float)
+    return R, t
+
+
+def get_or_create_dummy(name, size=0.012):
+    try:
+        return Dummy(name)
+    except Exception:
+        d = Dummy.create(size)
+        d.set_name(name)
+        return d
+    
+
+def mark_com_attached(shape: Shape, name_suffix="_COM_MARK", size=0.03):
+    h = shape.get_handle()
+    m, I, com_local, T = simGetShapeMassAndInertia(h)
+
+    d = get_or_create_dummy(shape.get_name() + name_suffix, size=size)
+    d.set_parent(shape)  # attach to the cracker
+    d.set_position(com_local.tolist(), relative_to=shape)  # local CoM
+    return com_local, m
+
+
+
+def to_jsonable(x):
+    """Recursively convert numpy types (ndarray, scalars) into JSON-serializable Python types."""
+    import numpy as np
+
+    if isinstance(x, dict):
+        return {k: to_jsonable(v) for k, v in x.items()}
+    if isinstance(x, (list, tuple)):
+        return [to_jsonable(v) for v in x]
+    if isinstance(x, np.ndarray):
+        return x.tolist()
+    if isinstance(x, (np.floating, np.integer)):
+        return x.item()
+    return x
+
+
+def tabletop_push_test(pyrep, obj: Shape, force=np.array([2.0, 0.0, 0.0]), steps=50):
+    # assume object already on table, settled
+    h = obj.get_handle()
+    p0 = np.array(obj.get_position())
+    o0 = np.array(obj.get_orientation())
+
+    for _ in range(steps):
+        sim.simAddForceAndTorque(h, force.tolist(), [0, 0, 0])
+        pyrep.step()
+
+    p1 = np.array(obj.get_position())
+    o1 = np.array(obj.get_orientation())
+    dp = np.linalg.norm(p1 - p0)
+    do = np.linalg.norm(o1 - o0)
+    return dp, do, p0, p1, o0, o1
+
+def try_collect_demo(task_env, attempts=3):
+    for k in range(attempts):
+        try:
+            (demo,) = task_env.get_demos(amount=1, live_demos=True)
+            return True, demo, None
+        except Exception as e:
+            err = repr(e)
+    return False, None, err
+
+
+
+
+
+
+
+
+
+def check_and_make(dir_path: str):
+    os.makedirs(dir_path, exist_ok=True)
+
+
+def save_demo(demo, example_path: str):
+    # Save image data first, and then None the image data, and pickle
+
+    left_shoulder_rgb_path = os.path.join(example_path, LEFT_SHOULDER_RGB_FOLDER)
+    left_shoulder_depth_path = os.path.join(example_path, LEFT_SHOULDER_DEPTH_FOLDER)
+    left_shoulder_mask_path = os.path.join(example_path, LEFT_SHOULDER_MASK_FOLDER)
+
+    right_shoulder_rgb_path = os.path.join(example_path, RIGHT_SHOULDER_RGB_FOLDER)
+    right_shoulder_depth_path = os.path.join(example_path, RIGHT_SHOULDER_DEPTH_FOLDER)
+    right_shoulder_mask_path = os.path.join(example_path, RIGHT_SHOULDER_MASK_FOLDER)
+
+    overhead_rgb_path = os.path.join(example_path, OVERHEAD_RGB_FOLDER)
+    overhead_depth_path = os.path.join(example_path, OVERHEAD_DEPTH_FOLDER)
+    overhead_mask_path = os.path.join(example_path, OVERHEAD_MASK_FOLDER)
+
+    wrist_rgb_path = os.path.join(example_path, WRIST_RGB_FOLDER)
+    wrist_depth_path = os.path.join(example_path, WRIST_DEPTH_FOLDER)
+    wrist_mask_path = os.path.join(example_path, WRIST_MASK_FOLDER)
+
+    front_rgb_path = os.path.join(example_path, FRONT_RGB_FOLDER)
+    front_depth_path = os.path.join(example_path, FRONT_DEPTH_FOLDER)
+    front_mask_path = os.path.join(example_path, FRONT_MASK_FOLDER)
+
+    for p in [
+        left_shoulder_rgb_path, left_shoulder_depth_path, left_shoulder_mask_path,
+        right_shoulder_rgb_path, right_shoulder_depth_path, right_shoulder_mask_path,
+        overhead_rgb_path, overhead_depth_path, overhead_mask_path,
+        wrist_rgb_path, wrist_depth_path, wrist_mask_path,
+        front_rgb_path, front_depth_path, front_mask_path
+    ]:
+        check_and_make(p)
+
+    for i, obs in enumerate(demo):
+        left_shoulder_rgb = Image.fromarray(obs.left_shoulder_rgb)
+        left_shoulder_depth = utils.float_array_to_rgb_image(
+            obs.left_shoulder_depth, scale_factor=DEPTH_SCALE)
+        left_shoulder_mask = Image.fromarray((obs.left_shoulder_mask * 255).astype(np.uint8))
+
+        right_shoulder_rgb = Image.fromarray(obs.right_shoulder_rgb)
+        right_shoulder_depth = utils.float_array_to_rgb_image(
+            obs.right_shoulder_depth, scale_factor=DEPTH_SCALE)
+        right_shoulder_mask = Image.fromarray((obs.right_shoulder_mask * 255).astype(np.uint8))
+
+        overhead_rgb = Image.fromarray(obs.overhead_rgb)
+        overhead_depth = utils.float_array_to_rgb_image(
+            obs.overhead_depth, scale_factor=DEPTH_SCALE)
+        overhead_mask = Image.fromarray((obs.overhead_mask * 255).astype(np.uint8))
+
+        wrist_rgb = Image.fromarray(obs.wrist_rgb)
+        wrist_depth = utils.float_array_to_rgb_image(
+            obs.wrist_depth, scale_factor=DEPTH_SCALE)
+        wrist_mask = Image.fromarray((obs.wrist_mask * 255).astype(np.uint8))
+
+        front_rgb = Image.fromarray(obs.front_rgb)
+        front_depth = utils.float_array_to_rgb_image(
+            obs.front_depth, scale_factor=DEPTH_SCALE)
+        front_mask = Image.fromarray((obs.front_mask * 255).astype(np.uint8))
+
+        left_shoulder_rgb.save(os.path.join(left_shoulder_rgb_path, IMAGE_FORMAT % i))
+        left_shoulder_depth.save(os.path.join(left_shoulder_depth_path, IMAGE_FORMAT % i))
+        left_shoulder_mask.save(os.path.join(left_shoulder_mask_path, IMAGE_FORMAT % i))
+
+        right_shoulder_rgb.save(os.path.join(right_shoulder_rgb_path, IMAGE_FORMAT % i))
+        right_shoulder_depth.save(os.path.join(right_shoulder_depth_path, IMAGE_FORMAT % i))
+        right_shoulder_mask.save(os.path.join(right_shoulder_mask_path, IMAGE_FORMAT % i))
+
+        overhead_rgb.save(os.path.join(overhead_rgb_path, IMAGE_FORMAT % i))
+        overhead_depth.save(os.path.join(overhead_depth_path, IMAGE_FORMAT % i))
+        overhead_mask.save(os.path.join(overhead_mask_path, IMAGE_FORMAT % i))
+
+        wrist_rgb.save(os.path.join(wrist_rgb_path, IMAGE_FORMAT % i))
+        wrist_depth.save(os.path.join(wrist_depth_path, IMAGE_FORMAT % i))
+        wrist_mask.save(os.path.join(wrist_mask_path, IMAGE_FORMAT % i))
+
+        front_rgb.save(os.path.join(front_rgb_path, IMAGE_FORMAT % i))
+        front_depth.save(os.path.join(front_depth_path, IMAGE_FORMAT % i))
+        front_mask.save(os.path.join(front_mask_path, IMAGE_FORMAT % i))
+
+        # We save the images separately, so set these to None for pickling.
+        obs.left_shoulder_rgb = None
+        obs.left_shoulder_depth = None
+        obs.left_shoulder_point_cloud = None
+        obs.left_shoulder_mask = None
+
+        obs.right_shoulder_rgb = None
+        obs.right_shoulder_depth = None
+        obs.right_shoulder_point_cloud = None
+        obs.right_shoulder_mask = None
+
+        obs.overhead_rgb = None
+        obs.overhead_depth = None
+        obs.overhead_point_cloud = None
+        obs.overhead_mask = None
+
+        obs.wrist_rgb = None
+        obs.wrist_depth = None
+        obs.wrist_point_cloud = None
+        obs.wrist_mask = None
+
+        obs.front_rgb = None
+        obs.front_depth = None
+        obs.front_point_cloud = None
+        obs.front_mask = None
+
+    # Save the low-dimension data
+    with open(os.path.join(example_path, LOW_DIM_PICKLE), 'wb') as f:
+        pickle.dump(demo, f)
+
+
+def make_obs_config(img_size, renderer: str) -> ObservationConfig:
+    obs_config = ObservationConfig()
+    obs_config.set_all(True)
+
+    obs_config.right_shoulder_camera.image_size = img_size
+    obs_config.left_shoulder_camera.image_size = img_size
+    obs_config.overhead_camera.image_size = img_size
+    obs_config.wrist_camera.image_size = img_size
+    obs_config.front_camera.image_size = img_size
+
+    # Store depth as 0 - 1
+    obs_config.right_shoulder_camera.depth_in_meters = False
+    obs_config.left_shoulder_camera.depth_in_meters = False
+    obs_config.overhead_camera.depth_in_meters = False
+    obs_config.wrist_camera.depth_in_meters = False
+    obs_config.front_camera.depth_in_meters = False
+
+    # Save masks as rgb encodings (not single channel)
+    obs_config.left_shoulder_camera.masks_as_one_channel = False
+    obs_config.right_shoulder_camera.masks_as_one_channel = False
+    obs_config.overhead_camera.masks_as_one_channel = False
+    obs_config.wrist_camera.masks_as_one_channel = False
+    obs_config.front_camera.masks_as_one_channel = False
+
+    if renderer == 'opengl':
+        mode = RenderMode.OPENGL
+    elif renderer == 'opengl3':
+        mode = RenderMode.OPENGL3
+    else:
+        raise ValueError(f"Unknown renderer: {renderer}")
+
+    obs_config.right_shoulder_camera.render_mode = mode
+    obs_config.left_shoulder_camera.render_mode = mode
+    obs_config.overhead_camera.render_mode = mode
+    obs_config.wrist_camera.render_mode = mode
+    obs_config.front_camera.render_mode = mode
+
+    return obs_config
+
+
+def collect_single_process(tasks, args):
+    img_size = list(map(int, args.image_size))
+    obs_config = make_obs_config(img_size, args.renderer)
+
+    rlbench_env = Environment(
+        action_mode=MoveArmThenGripper(JointVelocity(), Discrete()),
+        obs_config=obs_config,
+        arm_max_velocity=args.arm_max_velocity,
+        arm_max_acceleration=args.arm_max_acceleration,
+        headless=args.headless,
+    )
+    rlbench_env.launch()
+
+    tasks_with_problems = ""
+
+    try:
+        for task_id, t in enumerate(tasks):
+            task_env = rlbench_env.get_task(t)
+            task_name = task_env.get_name()
+
+            var_target = task_env.variation_count()
+            if args.variations >= 0:
+                var_target = int(np.minimum(args.variations, var_target))
+
+            print(f"\n=== Task {task_id+1}/{len(tasks)}: {task_name} | variations: {var_target} ===")
+
+            for var in range(var_target):
+                task_env.set_variation(var)
+                descriptions, _ = task_env.reset()
+                
+                task_env._ballast_mass_override = args.ballast_mass_override
+                task_env._ballast_offset_override = args.ballast_offset_override
+
+                # inject once per variation reset, same physics for all demos in this variation
+                physics_meta_for_variation = inject_task_physics(task_env)
+                
+                # ---------- start debugging ------ 
+                #import pdb; pdb.set_trace()
+                pyrep = rlbench_env._pyrep
+                g = [x for x in task_env._task.groceries if x.get_name() == 'crackers'][0]
+
+                com_local, com_world, m_eff = mark_com_attached(g)
+                print("[phys] crackers mass:", m_eff, "com_local:", com_local, "com_world:", com_world)
+
+                for _ in range(50):
+                    pyrep.step()
+                
+                # --------  1) visually confirm COM/mass
+                com_local, com_world, m_eff = mark_com_attached(g)
+                print("[phys] crackers mass:", m_eff, "com_local:", com_local, "com_world:", com_world)
+
+                # 2) quick stability test (optional)
+                for _ in range(50): pyrep.step()  # settle
+                dp, do, *_ = tabletop_push_test(pyrep, g, force=np.array([2.0,0,0]), steps=30)
+                print("[push] dp:", dp, "do:", do)
+
+                # 3) end-to-end: does scripted demo still succeed?
+                ok, demo, err = try_collect_demo(task_env, attempts=2)
+                print("[demo] success:", ok, "err:", err)
+
+                
+
+               
+                
+
+
+                variation_path = os.path.join(
+                    args.save_path, task_name, VARIATIONS_FOLDER % var
+                )
+                check_and_make(variation_path)
+
+                with open(os.path.join(variation_path, VARIATION_DESCRIPTIONS), 'wb') as f:
+                    pickle.dump(descriptions, f)
+
+                episodes_path = os.path.join(variation_path, EPISODES_FOLDER)
+                check_and_make(episodes_path)
+
+                abort_variation = False
+
+                for ex_idx in range(args.episodes_per_task):
+                    print(f"Task: {task_name} | Variation: {var} | Demo: {ex_idx}")
+
+                    attempts = args.max_attempts
+                    while attempts > 0:
+                        try:
+                            (demo,) = task_env.get_demos(amount=1, live_demos=True)
+                        except Exception as e:
+                            attempts -= 1
+                            if attempts > 0:
+                                continue
+
+                            problem = (
+                                f"Failed collecting task {task_name} "
+                                f"(variation: {var}, example: {ex_idx}). "
+                                f"Skipping this variation.\n{str(e)}\n"
+                            )
+                            print(problem)
+                            tasks_with_problems += problem
+                            abort_variation = True
+                            break
+
+                        episode_path = os.path.join(episodes_path, EPISODE_FOLDER % ex_idx)
+                        check_and_make(episode_path)
+
+                        save_demo(demo, episode_path)
+
+                        with open(os.path.join(episode_path, "physics.json"), "w") as f:
+                            json.dump(to_jsonable(physics_meta_for_variation), f, indent=2)
+
+
+                        break
+
+                    if abort_variation:
+                        break
+
+    finally:
+        rlbench_env.shutdown()
+
+    return tasks_with_problems
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="RLBench Dataset Generator (single-process debug)")
+    parser.add_argument('--save_path', type=str, default='/tmp/rlbench_data/',
+                        help='Where to save the demos.')
+    parser.add_argument('--tasks', nargs='*', default=[],
+                        help='The tasks to collect. If empty, all tasks are collected.')
+    parser.add_argument('--image_size', nargs=2, type=int, default=[128, 128],
+                        help='The size of the images to save.')
+    parser.add_argument('--renderer', type=str, choices=['opengl', 'opengl3'], default='opengl3',
+                        help='Renderer. opengl does not include shadows, but is faster.')
+    parser.add_argument('--episodes_per_task', type=int, default=10,
+                        help='The number of episodes to collect per task.')
+    parser.add_argument('--variations', type=int, default=-1,
+                        help='Number of variations to collect per task. -1 for all.')
+    parser.add_argument('--arm_max_velocity', type=float, default=1.0,
+                        help='Max arm velocity used for motion planning.')
+    parser.add_argument('--arm_max_acceleration', type=float, default=4.0,
+                        help='Max arm acceleration used for motion planning.')
+
+    # Debugging / determinism
+    parser.add_argument('--seed', type=int, default=1000,
+                        help='Random seed for physics injection RNG.')
+    parser.add_argument('--debug', action='store_true',
+                        help='Drop into pdb after each variation reset (right before inject_task_physics).')
+    parser.add_argument('--headless', action='store_true',
+                        help='Run headless (no GUI). Omit this flag for GUI debugging.')
+    parser.add_argument('--max_attempts', type=int, default=10,
+                        help='Retry attempts for demo collection per episode.')
+
+    parser.add_argument('--ballast_mass_override', type=float, default=None,
+                    help='If set, force ballast mass (kg) for all groceries to this value.')
+    parser.add_argument('--ballast_offset_override', nargs=3, type=float, default=None,
+                    help='If set, force ballast local offset (x y z) in meters.')
+
+
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    check_and_make(args.save_path)
+
+    task_files = [
+        t.replace('.py', '') for t in os.listdir(task.TASKS_PATH)
+        if t != '__init__.py' and t.endswith('.py')
+    ]
+
+    if len(args.tasks) > 0:
+        for t in args.tasks:
+            if t not in task_files:
+                raise ValueError(f"Task {t} not recognised!")
+        task_files = args.tasks
+
+    tasks = [task_file_to_task_class(t) for t in task_files]
+
+    problems = collect_single_process(tasks, args)
+
+    print("\nData collection done!")
+    if problems:
+        print("\nProblems encountered:\n")
+        print(problems)
+    else:
+        print("No problems encountered.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ rlbench/helper.py
@@ -0,0 +1,177 @@
+import json
+import trimesh
+import numpy as np
+from pyrep.objects.shape import Shape
+from pyrep.objects.dummy import Dummy
+from pyrep.const import PrimitiveShape
+
+from pyrep.backend import sim
+from pyrep.backend._sim_cffi import lib, ffi
+from pyrep.backend.sim import _check_return
+
+from typing import Any, Optional, Sequence, Union, Tuple, List, Dict
+Number = Union[int, float]
+
+# ---------- utilities  ---------- 
+def _S(d: np.ndarray) -> np.ndarray:
+    d = np.asarray(d, dtype=np.float64).reshape(3)
+    return (d @ d) * np.eye(3) - np.outer(d, d)
+
+def _get_T(handle: int) -> np.ndarray:
+    T = np.asarray(sim.simGetObjectMatrix(handle, -1), dtype=np.float32).reshape(-1)
+    assert T.size == 12
+    return T
+
+# ------------ sim wrappers  ------------ 
+
+def simGetShapeMassAndInertia(handle: int, transf: Optional[np.ndarray] = None):
+    h = int(handle)
+    c_mass = ffi.new("float[1]")
+    c_I = ffi.new("float[9]")
+    c_com = ffi.new("float[3]")
+
+    T = _get_T(handle) if transf is None else np.asarray(transf, np.float32).reshape(-1)
+    c_T = ffi.new("float[12]", T.tolist())
+
+    ret = lib.simGetShapeMassAndInertia(h, c_mass, c_I, c_com, c_T)
+    _check_return(ret)
+
+    mass = float(c_mass[0])
+    I = np.array([float(c_I[i]) for i in range(9)], dtype=np.float64).reshape(3, 3)
+    com = np.array([float(c_com[i]) for i in range(3)], dtype=np.float64)
+    return mass, I, com, T  # return T used (handy)
+
+
+def simSetShapeMassAndInertia(handle: int, mass: float, inertia9: np.ndarray, com3: np.ndarray,
+                             transf: Optional[np.ndarray] = None):
+    h = int(handle)
+    I = np.asarray(inertia9, dtype=np.float32).reshape(3, 3)
+    com = np.asarray(com3, dtype=np.float32).reshape(3)
+
+    c_I = ffi.new("float[9]", I.reshape(-1).tolist())
+    c_com = ffi.new("float[3]", com.tolist())
+
+    T = _get_T(handle) if transf is None else np.asarray(transf, np.float32).reshape(-1)
+    c_T = ffi.new("float[12]", T.tolist())
+
+    ret = lib.simSetShapeMassAndInertia(h, float(mass), c_I, c_com, c_T)
+    _check_return(ret)
+
+
+# -------------- reset the Mass, CoM and Frictions -------------- 
+def sample_point_inside_shape(shape, pitch=None, margin_ratio=0.05) -> np.ndarray:
+    V, F, _ = shape.get_mesh_data()
+    mesh = trimesh.Trimesh(vertices=V, faces=F, process=False)
+
+    bmin, bmax = mesh.bounds
+    extent = (bmax - bmin)
+    if pitch is None:
+        pitch = float(np.max(extent) / 80.0)
+        pitch = max(pitch, 1e-4)
+
+    vox = mesh.voxelized(pitch).fill()
+    pts = vox.points
+
+    margin = margin_ratio * extent
+    lo = bmin + margin
+    hi = bmax - margin
+    mask = np.all((pts >= lo) & (pts <= hi), axis=1)
+    pts = pts[mask]
+
+    if len(pts) == 0:
+        raise RuntimeError("No interior voxel points after margin filter; adjust pitch/margin.")
+    # return pts[np.random.randint(len(pts))]
+    d2 = np.sum(pts**2, axis=1)
+
+    # index of farthest point
+    idx = np.argmax(d2)
+
+    # the point itself
+    return pts[idx]
+
+
+def add_point_mass_ballast_and_set(shape, ballast_mass: Number, margin_ratio=0.05):
+    handle = shape.get_handle()
+    mb = float(ballast_mass)
+    if mb <= 0:
+        raise ValueError(f"ballast_mass must be > 0, got {mb}")
+
+    # -------- Get current mass, com, inertia  -------- 
+    m0, I0, com0, T = simGetShapeMassAndInertia(handle)  # T captured here
+
+    #  --------  Sample ballast point in the mesh local frame  -------- 
+    p_b = sample_point_inside_shape(shape, margin_ratio=margin_ratio)
+    
+    #  -------- compute new mass + CoM + inertia (wrt local frame)  -------- 
+    m = m0 + mb
+    com_new = (m0 * com0 + mb * p_b) / m
+    I_new = I0 + m0 * _S(com0 - com_new) + mb * _S(p_b - com_new)
+    I_new = 0.5 * (I_new + I_new.T)
+
+    simSetShapeMassAndInertia(handle, m, I_new, com_new, transf=T)
+
+    if hasattr(sim, "simResetDynamicObject"):
+        sim.simResetDynamicObject(handle)
+
+    # return effective values (what engine actually uses)
+    m_eff, I_eff, com_eff, _ = simGetShapeMassAndInertia(handle, transf=T)
+    return m_eff, I_eff, com_eff
+
+
+def inject_task_physics(task_env, rng=None, spec=None):
+    if rng is None:
+        rng = np.random
+
+    if spec is None:
+        spec = {"mass_frac": (0.05, 0.5), "mu": (0.2, 1.2)}
+
+    task = getattr(task_env, "_task", None)
+    if task is None:
+        return {"note": "no task handle", "objects": {}}
+
+    groceries = getattr(task, "groceries", None)
+    if groceries is None:
+        return {"note": "task has no groceries attribute; no injection applied", "objects": {}}
+
+    meta = {"note": "applied to groceries", "objects": {}}
+
+    # for g in groceries:
+    g = [g for g in task_env._task.groceries if g.get_name()=='crackers'][0]
+    
+    name = g.get_name()
+    h = g.get_handle()
+
+    # mass props baseline (source of truth)
+    m0_prop, _, _, _ = simGetShapeMassAndInertia(h)
+    m0 = float(m0_prop)
+    mu0 = float(g.get_bullet_friction())
+
+    # ballast
+    a_lo, a_hi = spec["mass_frac"]
+    alpha = float(rng.uniform(a_lo, a_hi))
+    alpha = 5.0            # -----------------
+    mb = alpha * m0
+
+    new_mass, new_I, new_com = add_point_mass_ballast_and_set(
+        g, ballast_mass=mb, margin_ratio=0.05
+    )
+
+    # friction
+    mu_lo, mu_hi = spec["mu"]
+    mu1 = float(rng.uniform(mu_lo, mu_hi))
+    g.set_bullet_friction(mu1)
+
+    meta["objects"][name] = {
+        "m0": m0,
+        "mu0": mu0,
+        "alpha": alpha,
+        "mb": float(mb),
+        "m1": float(new_mass),
+        "mu1": mu1,
+        "com1_local": np.asarray(new_com, dtype=float).reshape(3).tolist(),
+        "inertia1_local": np.asarray(new_I, dtype=float).reshape(-1).tolist(),
+        "inertia1_principal": np.linalg.eigvalsh(new_I).tolist(),
+        "inertia1_massless_principal": (np.linalg.eigvalsh(new_I) / float(new_mass)).tolist(),
+    }
+
+    return meta
